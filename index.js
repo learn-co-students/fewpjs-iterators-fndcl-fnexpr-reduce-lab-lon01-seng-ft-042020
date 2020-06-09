10514c8c239dc1d0a1d9164b7865420e2005b64f
Adao Natalino <adaojosen@hotmail.com>
2020-06-09 22:59:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce(function(total, batches){
+//     return batches + total
+//     }
+//   )
+
+let totalBatteries = batteryBatches.reduce((total, batches) => total + batches)
+
+//   let sum = data.reduce((acc, val) => {
+//     return val.country == 'China' ? acc : acc + val.pop;
+//   }, 0);
